b435cbfb517b3f833841602ca6ee41a863635df1
Dionis Silva Oliveira <dionis.oliveira@montreal.com.br>
2021-05-19 21:11:50 -0300

    Add new project to branch

---
+++ ICQ_Client/ICQ_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICQ_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ICQ_ManagerServer/ICQ_ManagerServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICQ_ManagerServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
